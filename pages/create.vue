e1e1f159784c20ae96cf9cd0bbb8b95d3b27d7c2
mortaen <kolbe.mor@gmail.com>
2021-12-10 10:34:23 +0100

    Add head to index, create and default, add .nuxt to git

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,19 @@
         <h1>Create an Event</h1>
       </div>
     </template>
+
+    <script>
+    export default {
+      head() {
+        return {
+          title: 'Create an Event',
+          meta: [
+          {
+            hid: 'descriptiom',
+            name: 'description',
+            content: 'You can create a new event in you neighborhood'
+          }]
+        }
+      }
+    }
+    </script>
