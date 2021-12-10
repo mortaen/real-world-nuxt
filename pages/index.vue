e3125da9f0143d1cbd2712d7409a0b0d3ed51b52
mortaen <kolbe.mor@gmail.com>
2021-12-10 12:28:31 +0100

    Add db.json & axios call & EventCard component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,39 @@
 <template>
-      <div>
-        <h1>Events</h1>
-      </div>
-    </template>
-    <script>
-    export default {
-      head() {
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard,
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
         return {
-          title: 'Event Listing',
+          events: response.data,
         }
-      }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Uable to fetch events at this time',
+        })
+      })
+  },
+  head() {
+    return {
+      title: 'Event Listing',
     }
-    </script>
+  },
+}
+</script>
